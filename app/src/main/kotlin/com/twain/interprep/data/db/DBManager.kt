4274d1d884ac0222fa7f844bab02a25dbff49fc4
Audienix <rontin0611@gmail.com>
2023-03-20 12:09:27 -0400

    Added few Room annotations.

--- app/src/main/kotlin/com/twain/interprep/data/db/DBManager.kt
+++ app/src/main/kotlin/com/twain/interprep/data/db/DBManager.kt
@@ -2,12 +2,10 @@ package com.twain.interprep.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
 import com.twain.interprep.data.dao.InterviewDAO
 import com.twain.interprep.data.model.Interview
 
 @Database(entities = [Interview::class], version = 1, exportSchema = false)
-@TypeConverters(Converters::class)
 abstract class DBManager : RoomDatabase() {
     abstract fun interviewDao(): InterviewDAO
 }
