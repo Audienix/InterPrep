c7828bb136c5338eff3af3bde31c724bfc29e664
Sarah Chung <sarah.chung@wattpad.com>
2023-03-23 13:42:02 -0400

    fix dimensions

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FullScreenEmptyState.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FullScreenEmptyState.kt
@@ -10,10 +10,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.twain.interprep.R
 
 @Composable
@@ -25,8 +25,7 @@ fun FullScreenEmptyState(modifier: Modifier,
         Column(
             modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(
-                16.dp,
-                // dimensionResource(R.dimen.padding_medium),
+                dimensionResource(R.dimen.dimension_16dp),
                 Alignment.CenterVertically
             ),
             horizontalAlignment = Alignment.CenterHorizontally
