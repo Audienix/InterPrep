003f40ef918eb8b18d589a7fafd1ec25518fffc6
Sarah Chung <60327675+sarahhjchung@users.noreply.github.com>
2023-03-29 15:36:18 -0400

    Implement FullScreenEmptyState component (#2)
    * add FullScreenEmptyState component
    
    * fix dimensions

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FullScreenEmptyState.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FullScreenEmptyState.kt
@@ -5,9 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,24 +13,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.twain.interprep.R
 
 @Composable
-fun FullScreenEmptyState(
-    modifier: Modifier = Modifier,
-    imageId: Int,
-    emptyStateTitle: String = "",
-    emptyStateText: String = ""
-) {
+fun FullScreenEmptyState(modifier: Modifier,
+                         imageId: Int,
+                         emptyStateTitle: String = "",
+                         emptyStateText: String = "") {
     Box {
         Column(
-            modifier = modifier
-                .fillMaxSize()
-                .padding(horizontal = dimensionResource(id = R.dimen.dimension_16dp)),
+            modifier = modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(
                 dimensionResource(R.dimen.dimension_16dp),
                 Alignment.CenterVertically
@@ -41,57 +31,22 @@ fun FullScreenEmptyState(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                //TODO change constant height value
-                modifier = Modifier
-                    .height(240.dp)
-                    .width(290.dp),
                 painter = painterResource(id = imageId),
-                contentDescription = "No data"
+                contentDescription = "Full Screen Empty State Image"
             )
-            Text(
-                text = emptyStateTitle,
-                fontSize = MaterialTheme.typography.titleLarge.fontSize,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center
-            )
-            Text(
-                text = emptyStateText,
-                style = MaterialTheme.typography.titleMedium,
-                textAlign = TextAlign.Center
-            )
+            Text(text = emptyStateTitle, style = MaterialTheme.typography.bodyLarge)
+            Text(text = emptyStateText, style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun FullScreenEmptyStateInterview() {
+private fun FullScreenEmptyStatePreview() {
     FullScreenEmptyState(
         Modifier,
         R.drawable.empty_state_dashboard,
-        stringResource(id = R.string.empty_state_title_dashboard),
-        stringResource(id = R.string.empty_state_description_dashboard)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun FullScreenEmptyStateResource() {
-    FullScreenEmptyState(
-        Modifier,
-        R.drawable.empty_state_resource,
-        stringResource(id = R.string.empty_state_title_resource),
-        stringResource(id = R.string.empty_state_description_resource)
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun FullScreenEmptyStateNotes() {
-    FullScreenEmptyState(
-        Modifier,
-        R.drawable.empty_state_notes,
-        stringResource(id = R.string.empty_state_title_note),
-        stringResource(id = R.string.empty_state_description_note)
+        stringResource(id = R.string.dashboard_empty_title),
+        stringResource(id = R.string.dashboard_empty_text)
     )
 }
