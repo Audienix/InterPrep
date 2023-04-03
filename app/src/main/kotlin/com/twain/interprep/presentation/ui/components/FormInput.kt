aa734d5ff45d0a86c4e59c71a2e2e3b4c77da832
Sarah Chung <sarah.chung@wattpad.com>
2023-04-03 14:48:27 -0400

    add dropdown menu component

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FormInput.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/FormInput.kt
@@ -1,63 +1,97 @@
 package com.twain.interprep.presentation.ui.components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.res.painterResource
-//import androidx.compose.ui.res.stringResource
-//import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-//import com.twain.interprep.R
+import com.twain.interprep.R
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInput(
-//    modifier: Modifier,
+    modifier: Modifier,
     value: String,
-    onValueChange: (String) -> Unit,
     labelText: String,
-//    bottomText: String = "",
+    bottomText: String = "",
     required: Boolean = false,
-//    isError: Boolean,
-//    errorMessage: String = "",
-    // options: List<String>? = null
+    errorMessage: String = "",
 ) {
     val label = if (required) "$labelText *" else labelText
+    var text by remember { mutableStateOf(value) }
+    var isError by remember { mutableStateOf(false) }
+
+    fun validate(text: String) {
+        isError = (required && text == "")
+    }
 
     OutlinedTextField(
-        value = value,
-        onValueChange = onValueChange,
-//        modifier = modifier,
+        value = text,
+        onValueChange = {
+            text = it
+            validate(text)
+        },
+        modifier = modifier,
         singleLine = true,
         label = { Text(text = label) },
-//        isError = isError,
-//        supportingText = {
-//            if (isError) {
-//                Text(text = errorMessage, color = MaterialTheme.colorScheme.error)
-//            } else {
-//                Text(text = bottomText)
-//            }
-//        },
-//        trailingIcon = {
-//            if (isError)
-//                Icon(painter = painterResource(id = R.drawable.error_icon),
-//                     contentDescription = "Error")
-//        },
+        isError = isError,
+        keyboardActions = KeyboardActions { validate(text) },
+        supportingText = {
+            if (isError) {
+                Text(
+                    modifier = modifier.padding(
+                        bottom = dimensionResource(id = R.dimen.dimension_8dp)),
+                    text = errorMessage,
+                    color = MaterialTheme.colorScheme.error)
+            } else if (bottomText != "") {
+                Text(
+                    modifier = modifier.padding(
+                        bottom = dimensionResource(id = R.dimen.dimension_8dp)),
+                    text = bottomText)
+            }
+        },
+        trailingIcon = {
+            if (isError)
+                Icon(painter = painterResource(id = R.drawable.error_icon),
+                     contentDescription = "Error")
+        },
     )
 }
 
-@Preview(showBackground = true, backgroundColor = 0xFFFFFF)
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFFFFFF)
 @Composable
 private fun FormInputPreview() {
     var text by remember { mutableStateOf("") }
+    var text2 by remember { mutableStateOf("") }
 
-    FormInput(
-//        modifier = Modifier,
-        value = text,
-        onValueChange = { text = it },
-        labelText = "Company",
-        required = true,
-//        isError = false,
-//        errorMessage = stringResource(id = R.string.form_input_error_message)
-    )
+    val options = listOf<String>("Recruiter", "Hiring Manager", "Technical", "Behavioral")
+
+    Column() {
+        FormInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = text,
+            labelText = "Company",
+            required = true,
+            errorMessage = stringResource(id = R.string.form_input_error_message)
+        )
+        FormInput(
+            modifier = Modifier.fillMaxWidth(),
+            value = text2,
+            labelText = "Name",
+            required = false
+        )
+        DropdownMenu(
+            modifier = Modifier.fillMaxWidth(),
+            options = options,
+            labelText = "Interview Type"
+        )
+    }
 }
