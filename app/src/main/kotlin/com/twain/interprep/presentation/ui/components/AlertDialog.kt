f1a9d5838f3347a87361ec08cac25eeb97cc8514
Sarah Chung <sarah.chung@wattpad.com>
2023-04-13 11:40:39 -0400

    add alert dialog component

---
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/AlertDialog.kt
@@ -0,0 +1,56 @@
+package com.twain.interprep.presentation.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.twain.interprep.R
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AlertDialog(
+    title: String,
+    text: String,
+    positiveButtonText: String = stringResource(id = R.string.alert_dialog_positive_button),
+    negativeButtonText: String = stringResource(id = R.string.alert_dialog_negative_button),
+    onPositiveButtonClick: () -> Unit,
+    onNegativeButtonClick: () -> Unit,
+    onDismiss: () -> Unit = { }
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = title)
+        },
+        text = {
+            Text(text = text)
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onPositiveButtonClick
+            ) {
+                Text(text = positiveButtonText)
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onNegativeButtonClick
+            ) {
+                Text(text = negativeButtonText)
+            }
+        }
+    )
+}
+
+
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun AlertDialogPreview() {
+    AlertDialog(
+        title = stringResource(id = R.string.alert_dialog_unsaved_interview_title),
+        text = stringResource(id = R.string.alert_dialog_unsaved_interview_text),
+        onPositiveButtonClick = { /*TODO*/ },
+        onNegativeButtonClick = { /*TODO*/ }
+    )
+}
