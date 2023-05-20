77cf3cf025154f8552a27797bda1eeb82154b4bb
Yanling Hu <67870437+YANLING-H@users.noreply.github.com>
2023-05-19 23:27:09 -0400

    Feature -  Finish creating "Add interview" screen (#7)
    * finish
    
    * fix the scrolling issue
    
    * change data structure input into TextFormInput
    
    * Handled Claer button click in TextFormInput
    
    ---------
    
    Co-authored-by: Audienix <rontin0611@gmail.com>

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/TextFormInput.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/TextFormInput.kt
@@ -13,28 +13,28 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.twain.interprep.R
+import com.twain.interprep.utils.Input
 import com.twain.interprep.utils.validateRequiredField
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TextFormInput(
-    modifier: Modifier,
-    value: String = "",
-    labelText: String,
-    bottomText: String = "",
-    required: Boolean = false,
-    errorText: String = "",
+    modifier: Modifier = Modifier,
+    input: Input,
 ) {
-    val label = if (required) "$labelText *" else labelText
-    var text by remember { mutableStateOf(value) }
+    val labelText = stringResource(id = input.labelTextId)
+    val bottomText = input.bottomTextId?.let { stringResource(id = it) } ?: ""
+    val errorText = input.errorTextId?.let { stringResource(id = it) } ?: ""
+
+    val label = if (input.required) "$labelText *" else labelText
+    var text by remember { mutableStateOf(input.value) }
     var isError by remember { mutableStateOf(false) }
 
     OutlinedTextField(
         value = text,
         onValueChange = {
             text = it
-            if (required) isError = validateRequiredField(text)
+            if (input.required) isError = validateRequiredField(text)
         },
         modifier = modifier,
         singleLine = true,
@@ -58,7 +58,10 @@ fun TextFormInput(
                 )
             } else if (text.isNotEmpty()) {
                 IconButton(
-                    onClick = { /* TODO: implement onClick for clearing text */ }
+                    onClick = {
+                        text = ""
+                        if (input.required) isError = validateRequiredField(text)
+                    }
                 ) {
                     Icon(
                         painter = painterResource(id = R.drawable.cancel_icon),
@@ -83,21 +86,19 @@ private fun TextFormInputPreview() {
     ) {
         TextFormInput(
             modifier = Modifier.fillMaxWidth(),
-            labelText = "Company",
-            required = true,
-            errorText = stringResource(id = R.string.error_message_form_input),
+            input = Input(
+                labelTextId = R.string.hint_label_company,
+                required = true,
+                errorTextId = R.string.error_message_form_input
+            )
         )
         TextFormInput(
             modifier = Modifier.fillMaxWidth(),
-            value = "Sarah",
-            labelText = "Name",
-            required = false
-        )
-        TextFormInput(
-            modifier = Modifier.fillMaxWidth(),
-            labelText = "Date",
-            bottomText = "MM/YY",
-            required = false
+            input = Input(
+                labelTextId = R.string.hint_label_date,
+                bottomTextId = R.string.hint_label_month_format,
+                required = false
+            )
         )
         DropdownMenuInput(
             modifier = Modifier.fillMaxWidth(),
