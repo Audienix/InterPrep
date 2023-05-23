29780370b4c45cdc1b528eb3c5f0ab5b753904b0
Audienix <rontin0611@gmail.com>
2023-05-22 21:35:22 -0400

    Added missing file

---
+++ app/src/main/kotlin/com/twain/interprep/helper/Constants.kt
@@ -0,0 +1,13 @@
+package com.twain.interprep.helper
+
+class Constants {
+    companion object {
+        const val DT_FORMAT_DATE = "dd"
+        const val DT_FORMAT_MONTH_YEAR = "MMM, yyyy"
+        const val DT_FORMAT_DAY_HOUR_MIN = "EEE, hh:mm"
+        const val DT_FORMAT_AM_PM = "a"
+        const val DT_FORMAT_MM_DD_YYYY = "MM/dd/yyyy"
+
+        const val DB_TABLE_INTERVIEWS = "interviews"
+    }
+}
