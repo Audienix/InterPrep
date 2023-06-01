c0eb1a40ae9940436b52bc58e3f91437d7ceffe3
Audienix <rontin0611@gmail.com>
2023-06-01 00:56:35 -0400

    Added QuoteCard

---
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/generic/IPQuoteCard.kt
@@ -0,0 +1,72 @@
+package com.twain.interprep.presentation.ui.components.generic
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.twain.interprep.R
+import com.twain.interprep.data.model.Quote
+import com.twain.interprep.data.ui.QuoteData
+import com.twain.interprep.presentation.ui.theme.Shapes
+import com.twain.interprep.presentation.ui.theme.TextPrimary
+import com.twain.interprep.presentation.ui.theme.TextSecondary
+
+@Composable
+fun IPQuoteCard(modifier: Modifier = Modifier, quote: Quote) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(dimensionResource(id = R.dimen.dimension_4dp)),
+        colors = CardDefaults.elevatedCardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp),
+        shape = Shapes.extraSmall,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.dimension_16dp)),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = quote.quote,
+                color = TextPrimary,
+                style = MaterialTheme.typography.bodyLarge,
+                fontStyle = FontStyle.Italic
+            )
+            Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.dimension_4dp)))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "~ ${quote.author}",
+                color = TextSecondary,
+                style = MaterialTheme.typography.bodyMedium,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.End
+            )
+        }
+    }
+}
+
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun QuoteCardPreview() {
+    IPQuoteCard(quote = QuoteData.quotes[0])
+}
---
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/interview/InterviewDetailsCard.kt
@@ -0,0 +1,9 @@
+package com.twain.interprep.presentation.ui.components.interview
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.twain.interprep.data.model.Interview
+
+@Composable
+fun InterviewDetailsCard(modifier: Modifier = Modifier, interview: Interview) {
+}
