74fb0d527f1155be9bc9bc18e384931d52e10d59
Xuhui Chen <severusheep9@hotmail.com>
2023-06-15 23:34:39 -0400

    remove close icon when the field does not have focus

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/interview/IPTextInput.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/interview/IPTextInput.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.layout.onGloballyPositioned
@@ -49,13 +50,14 @@ fun IPTextInput(
     var isError by remember { mutableStateOf(false) }
     val source = remember { MutableInteractionSource() }
     var textFieldSize by remember { mutableStateOf(Size.Zero) }
-//    var text by remember { mutableStateOf(inputText) }
+
+    var hasFocus by remember { mutableStateOf(false) }
 
     OutlinedTextField(
         modifier = modifier.onGloballyPositioned { coordinates ->
             //This value is used to assign to the DropDown the same width
             textFieldSize = coordinates.size.toSize()
-        },
+        }.onFocusChanged { hasFocus = it.hasFocus },
         value = inputText,
         onValueChange = {
             onTextUpdate(it)
@@ -81,7 +83,7 @@ fun IPTextInput(
                     painter = painterResource(id = R.drawable.error_icon),
                     contentDescription = "Error"
                 )
-            } else if (inputText.isNotEmpty()) {
+            } else if (inputText.isNotEmpty() && hasFocus) {
                 IconButton(
                     onClick = {
                         onTextUpdate("")
