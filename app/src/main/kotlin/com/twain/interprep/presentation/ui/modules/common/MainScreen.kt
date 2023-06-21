7c825092ef9880fbc8d10bf29f3840da0a51151b
Sarah Chung <sarah.chung@wattpad.com>
2023-06-21 16:19:04 -0400

    Shift insertQuotes into MainScreen from DashboardScreen

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/modules/common/MainScreen.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/modules/common/MainScreen.kt
@@ -5,15 +5,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.twain.interprep.data.ui.QuoteData
 import com.twain.interprep.presentation.navigation.BottomNavigationBar
 import com.twain.interprep.presentation.navigation.NavGraph
+import com.twain.interprep.presentation.ui.modules.interview.QuotesViewModel
 
 @Composable
-fun MainScreen() {
+fun MainScreen(
+    quotesViewModel: QuotesViewModel = hiltViewModel(),
+) {
     val navController = rememberNavController()
+
+    LaunchedEffect(Unit) {
+        // TODO: check if this should be MainScreen or MainActivity
+        quotesViewModel.insertQuotes(QuoteData.quotes)
+    }
+
     Scaffold(
         bottomBar = {
             BottomNavigationBar(navController)
