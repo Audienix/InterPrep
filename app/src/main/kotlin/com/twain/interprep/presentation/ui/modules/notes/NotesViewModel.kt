81a2185c30b5519f34ad24dc7ac240650e341943
Prashanta Halder <halderp@meta.com>
2023-08-03 11:01:32 -0700

    Delete Question Implementation
    This commit deletes the question from Interview notes. Not all the questions can be deleted. There should be atleast 1 question necessary.

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/modules/notes/NotesViewModel.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/modules/notes/NotesViewModel.kt
@@ -101,6 +101,19 @@ class NotesViewModel @Inject constructor(
         )
     }
 
+    fun deleteQuestion(notesIndex:Int, questionIndex: Int) {
+        val note = notes[notesIndex]
+        // Check if this is the last question before deletion
+        // Additional check in Viewmodel for not deleting all questions
+        if (note.questions.size <= 1)
+            return;
+        val questions = note.questions.toMutableList()
+        questions.removeAt(questionIndex)
+        notes[notesIndex] = note.copy(
+            questions = questions.toList()
+        )
+    }
+
     fun addNote() = launchCoroutineIO {
         if (interview !is ViewResult.Loaded) return@launchCoroutineIO
 
