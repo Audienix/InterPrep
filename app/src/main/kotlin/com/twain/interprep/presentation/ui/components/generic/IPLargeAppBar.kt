86bd6c43c6d33acf25ec78359b4ebb1c042c25bc
patrick-zhou-wattpad <patrick.zhou@wattpad.com>
2024-02-15 21:24:14 -0500

    add blink text when count down timer is less than 1 hour

--- app/src/main/kotlin/com/twain/interprep/presentation/ui/components/generic/IPLargeAppBar.kt
+++ app/src/main/kotlin/com/twain/interprep/presentation/ui/components/generic/IPLargeAppBar.kt
@@ -84,6 +84,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.compose.animation.core.*
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.ui.draw.alpha
 
 @Composable
 fun IPLargeAppBar(
@@ -168,7 +171,23 @@ private fun GreetingsAndProfile(
 @Composable
 fun countTimer(timeString: String) {
     var remainingTime by remember { mutableStateOf("") }
+    var shouldBlink by remember { mutableStateOf(false)}
     val context = LocalContext.current
+
+    // Define animation for blinking effect when less than 1 hour remains
+    val blinkAnimation = rememberInfiniteTransition(label = "")
+    val animatedAlpha by blinkAnimation.animateFloat(
+        initialValue = 0f,
+        targetValue = 1f,
+        animationSpec = infiniteRepeatable(
+            animation = keyframes {
+                durationMillis = 1000  // Duration of one cycle (1000 ms = 1 second)
+                0.0f at 0 with LinearEasing
+                1.0f at 500 with LinearEasing  // Mid-point at half duration
+            },
+            repeatMode = RepeatMode.Reverse
+        ), label = ""
+    )
 
     fun getRemainingTime(targetTime: Long): String {
         val currentTime = System.currentTimeMillis()
@@ -179,11 +198,12 @@ fun countTimer(timeString: String) {
         val seconds = diff / 1000
         val minutes = seconds / 60
         val hours = minutes / 60
-        if (hours >=1){
-            return String.format("%02d:%02d", hours, minutes % 60)
-        }
-        else{
-            return String.format("%02d:%02d", minutes % 60, seconds)
+        if (hours < 1) {
+            shouldBlink = true
+            return String.format("%02dmins" +" %02ds", minutes % 60, seconds % 60)
+        } else {
+            shouldBlink = false
+            return String.format("%02dh" +" %02dmins", hours, minutes % 60)
         }
     }
 
@@ -211,21 +231,23 @@ fun countTimer(timeString: String) {
     if (remainingTime != "Time is up") {
         Spacer(
             modifier = Modifier.width(
-                dimensionResource(id = R.dimen.dimension_32dp) +
-                    dimensionResource(id = R.dimen.dimension_24dp)
+                dimensionResource(id = R.dimen.dimension_14dp)
             )
         )
     } else {
         Spacer(
             modifier = Modifier.width(
-                dimensionResource(id = R.dimen.dimension_24dp)
+                dimensionResource(id = R.dimen.dimension_20dp) +
+                    dimensionResource(id = R.dimen.dimension_2dp)
             )
         )
         showTimeUpNotification(context)
     }
+
     Text(
-        text = "Remaining: $remainingTime",
-        color = MaterialColorPalette.onPrimary,
+        modifier = Modifier.alpha(if (shouldBlink) animatedAlpha else 1f),
+        text = stringResource(id = R.string.remaining_time) + ": $remainingTime",
+        color = MaterialColorPalette.primaryContainer,
         style = MaterialTheme.typography.bodyMedium,
     )
 }
@@ -470,7 +492,7 @@ fun TodayInterviewCard(
                                 LocalContext.current,
                                 interview.time
                             ),
-                            color = MaterialColorPalette.onPrimary,
+                            color = MaterialColorPalette.primaryContainer,
                             style = MaterialTheme.typography.bodyMedium
                         )
 
@@ -526,8 +548,8 @@ fun TodayInterviewCard(
                                 modifier = Modifier.padding(end = dimensionResource(id = R.dimen.dimension_8dp)),
                                 text = stringResource(id = R.string.label_join_here),
                                 link = interview.meetingLink,
-                                textColor = MaterialColorPalette.onPrimary,
-                                textStyle = MaterialTheme.typography.bodyLarge,
+                                textColor = MaterialColorPalette.onSecondary,
+                                textStyle = MaterialTheme.typography.bodyMedium,
                             )
                         }
                     }
